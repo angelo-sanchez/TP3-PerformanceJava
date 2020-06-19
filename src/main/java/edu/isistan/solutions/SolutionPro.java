fc9af9202f24fa03f9a3c2f4f804a98b24ef80ec
Leandro <leandroperezb@gmail.com>
2020-06-19 14:35:31 -0300

    Repeat counting

--- src/main/java/edu/isistan/solutions/SolutionPro.java
+++ src/main/java/edu/isistan/solutions/SolutionPro.java
@@ -14,9 +14,24 @@ public class SolutionPro implements IProblemSolver {
 		Arrays.parallelSort(data);
 		
 		for (int i = 0; i < data.length; i++) {
+			if (i > 0 && data[i] == data[i-1])
+				continue;
 			int j = Arrays.binarySearch(data, sum - data[i]);
-				if (j > i)
-					pairs.add(new Pair(data[i], data[j]));
+				if (j > i) {
+					int cantI = 1;
+					int cantJ = 1;
+					for (int aux = i + 1; aux < data.length && data[aux] == data[i]; aux++) {
+						cantI++;
+					}
+					for (int aux = j + 1; aux < data.length && data[aux] == data[j]; aux++) {
+						cantJ++;
+					}
+					for (int aux = j - 1; aux >= 0 && data[aux] == data[j]; aux--) {
+						cantJ++;
+					}
+					for (int aux = 0; aux < cantI * cantJ; aux++)
+						pairs.add(new Pair(data[i], data[j]));
+				}
 		}
 
 		return pairs;
