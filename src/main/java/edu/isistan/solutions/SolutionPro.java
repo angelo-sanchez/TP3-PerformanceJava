dde6fef7d7f0a2d40c1728ca320eb6052d15eb44
Leandro <leandroperezb@gmail.com>
2020-06-21 13:47:28 -0300

    Use more meaningful words and extracted repetitive calculations

--- src/main/java/edu/isistan/solutions/SolutionPro.java
+++ src/main/java/edu/isistan/solutions/SolutionPro.java
@@ -14,30 +14,32 @@ public class SolutionPro implements IProblemSolver {
 		Arrays.parallelSort(data);
 
 		for (int i = 0; i < data.length; i++) {
-			if (i > 0 && data[i] == data[i - 1])
+			final int currentValue = data[i];
+			if (i > 0 && currentValue == data[i - 1])
 				continue;
 
 			// If the current value needs a lower value to make a pair, then the loop must
 			// finish because it's an ordered array
-			if (data[i] > sum - data[i])
+			final int complementaryValue = sum - currentValue;
+			if (currentValue > complementaryValue)
 				break;
 
-			if (sum == data[i] * 2) {
-				int cantI = frequencyOfI(data, i);
+			if (sum == currentValue * 2) {
+				int cantI = frequencyOfCurrentValue(data, i);
 				if (cantI > 1) {
 					// If the pair is with itself and it's a repeated value
 					int combinations = sum(cantI);
 					for (int aux = 0; aux < combinations; aux++)
-						pairs.add(new Pair(data[i], data[i]));
+						pairs.add(new Pair(currentValue, currentValue));
 				}
 			} else {
-				int j = Arrays.binarySearch(data, i + 1, data.length, sum - data[i]);
+				int j = Arrays.binarySearch(data, i + 1, data.length, complementaryValue);
 				if (j > 0) {
-					int cantI = frequencyOfI(data, i);
-					int cantJ = frequencyForJ(data, j);
-					// Add as many pairs as combinations of cantI with cantJ
-					for (int aux = 0; aux < cantI * cantJ; aux++)
-						pairs.add(new Pair(data[i], data[j]));
+					int freqCurrentValue = frequencyOfCurrentValue(data, i);
+					int freqComplementaryValue = frequencyOfComplementaryValue(data, j);
+					// Add as many pairs as combinations of the frequencies
+					for (int aux = 0; aux < freqCurrentValue * freqComplementaryValue; aux++)
+						pairs.add(new Pair(currentValue, complementaryValue));
 				}
 			}
 		}
@@ -49,20 +51,22 @@ public class SolutionPro implements IProblemSolver {
 		return (n * (n - 1)) / 2;
 	}
 
-	private int frequencyForJ(int[] data, int j) {
+	private int frequencyOfComplementaryValue(int[] data, int index) {
 		int cantJ = 1;
-		for (int aux = j + 1; aux < data.length && data[aux] == data[j]; aux++) {
+		final int currentValue = data[index];
+		for (int aux = index + 1; aux < data.length && data[aux] == currentValue; aux++) {
 			cantJ++;
 		}
-		for (int aux = j - 1; aux >= 0 && data[aux] == data[j]; aux--) {
+		for (int aux = index - 1; aux >= 0 && data[aux] == currentValue; aux--) {
 			cantJ++;
 		}
 		return cantJ;
 	}
 
-	private int frequencyOfI(int[] data, int i) {
+	private int frequencyOfCurrentValue(int[] data, int index) {
 		int cantI = 1;
-		for (int aux = i + 1; aux < data.length && data[aux] == data[i]; aux++) {
+		final int currentValue = data[index];
+		for (int aux = index + 1; aux < data.length && data[aux] == currentValue; aux++) {
 			cantI++;
 		}
 		return cantI;
